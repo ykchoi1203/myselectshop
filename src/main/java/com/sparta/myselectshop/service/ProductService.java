bf213edf3f9759a19af55b182c606ad481de3334
younggeun <chldudrms94@naver.com>
2024-11-05 12:43:39 +0900

    폴더 별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -115,4 +115,15 @@ public class ProductService {
 
 
     }
+
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+
+        return productList.map(ProductResponseDto::new);
+    }
 }
